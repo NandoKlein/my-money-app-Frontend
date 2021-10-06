ef5cd28e54aaf04f303471e7bfc315faae649154
Fernando klein silva <fernando.silva@qualyteam.com.br>
2021-10-06 09:26:38 -0300

    quarto dia

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2021
+            <a href="http://co3r.com.br" target='_blank'>Cod3r</a>.
+        </strong>
+    </footer>
+)
